b89b226ca8466ab701e3e8e85f813a7c518c5582
Hải Lê <dark.hades.1102@gmail.com>
2016-07-01 21:53:36 +0900

    To all os

--- CRLFtoLF.php
+++ CRLFtoLF.php
@@ -1,14 +1,31 @@
 <?php
-$ar = array();
-exec("find", $ar );
+function getDirContents($dir, &$results = array()){
+    $files = scandir($dir);
+
+    foreach($files as $key => $value){
+        $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
+        if(!is_dir($path)) {
+            $results[] = $path;
+        } else if($value != "." && $value != "..") {
+            getDirContents($path, $results);
+            $results[] = $path;
+        }
+    }
+
+    return $results;
+}
+$ar = getDirContents('./');
 foreach ( $ar as $file )
 {
-  if (mb_ereg(".php|.phtml",$file))
+  if (mb_ereg(".php",$file))
   {
-    if (mb_ereg("/lib/",$file))
+    if (mb_ereg("vendor",$file))
     {
       continue;
     }
+	if (is_dir(file)) {
+		continue;
+	}
     $fileContents = file_get_contents($file);
     $isCRLF = preg_match("/\r\n/", $fileContents);
     if ($isCRLF)
@@ -19,4 +36,3 @@ foreach ( $ar as $file )
     }
   }
 }
-?>
