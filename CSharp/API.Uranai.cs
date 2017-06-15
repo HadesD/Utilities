6f5c6487a05db6022d96e3875022d84a8c27b3ed
Hải Lê <dark.hades.1102@gmail.com>
2017-06-15 12:03:19 +0900

    Create API.Uranai.cs

---
+++ CSharp/API.Uranai.cs
@@ -0,0 +1,12 @@
+namespace API
+{
+  public class Hello
+  {
+    public static void Main()
+    {
+      // Here your code !
+            
+      System.Console.WriteLine("Hello C#");
+    }
+  }
+}
