83e64fdecff7d3a6305aa814837f283068143d56
Hải Lê <dark.hades.1102@gmail.com>
2017-07-01 20:02:28 +0900

    Update CheckIsPrime.cpp

--- CPP/CheckIsPrime.cpp
+++ CPP/CheckIsPrime.cpp
@@ -1,10 +1,32 @@
-#include <iostream>
+#include <math.h>
 
 bool isPrime(int n)
 {
-    if (n < 2)
+    if (n <= 1)
     {
         return false;
+    }
+    
+    if (n == 2)
+    {
+        return true;
+    }
+    
+    if ((n > 2) && ((n % 2) == 0))
+    {
+        return false;
+    }
+    
+    int to_max = sqrt(n);
+    for (int i=3; i<=to_max; i+=2)
+    {
+        if ((n % i) == 0)
+        {
+            if ((n % i) == 0)
+            {
+                return false;
+            }
+        }
     }
     
     return true;
@@ -12,16 +34,21 @@ bool isPrime(int n)
 
 
 // Test
+#include <iostream>
 #include <limits.h>
+#include <ctime>
 int main(void)
 {
     using namespace std;
-    int from = 1;//INT_MIN;
-    int to = 100;//INT_MAX;
+    int from = 0;
+    int to = 10;
+    const long double t0 = time(0)*1000;
     for (int i=from; i<=to; i++)
     {
         cout << i << " " << isPrime(i) << endl;
     }
+    const long double t = time(0)*1000;
+    cout << (t-t0);
     
     return 0;
 }
