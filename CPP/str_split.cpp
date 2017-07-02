864b4f7af5292a1c923c10885be9100ba04d8cb2
Hải Lê <dark.hades.1102@gmail.com>
2017-07-02 21:08:13 +0900

    Create str_split.cpp

---
+++ CPP/str_split.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <string>
+#include <sstream>
+
+std::vector<std::string> str_split(std::string str, char delimiter)
+{
+  std::vector<std::string> internal;
+  std::stringstream ss(str); // Turn the string into a stream.
+  std::string tok;
+  
+  while(std::getline(ss, tok, delimiter))
+  {
+    internal.push_back(tok);
+  }
+  
+  return internal;
+}
+
+// I don't recommend using the std namespace in production code.
+// For ease of reading here.
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv) {
+  string myCSV = "one,two,three,four";
+  vector<string> sep = str_split(myCSV, ',');
+
+  // If using C++11 (which I recommend)
+  /* for(string t : sep)
+   *  cout << t << endl;
+   */
+   
+  for(int i = 0; i < sep.size(); ++i)
+    cout << sep[i] << endl;
+}
