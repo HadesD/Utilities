23eccc8c137bc68fd5071e09f43b4ee8e5e89dd0
Hải Lê <dark.hades.1102@gmail.com>
2017-07-05 18:25:28 +0900

    Create preg_match.cpp

---
+++ CPP/preg_match.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <regex>
+
+bool preg_match(std::string pattern, std::string subject, std::smatch &matches)
+{
+  std::regex reg(pattern);
+  
+  if (std::regex_search(subject, matches, reg))
+  {
+    return true;
+  }
+  return false;
+}
+
+// Test
+#include <iostream>
+using namespace std;
+int main(void)
+{
+  smatch m;
+  preg_match("(\\d+)", "123sdflk", m);
+  cout << m[1];
+}
