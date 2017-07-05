fe476071ff6c4591551116d1b51f4f361817a8b0
Hải Lê <dark.hades.1102@gmail.com>
2017-07-05 18:49:29 +0900

    Overload vector search function

--- CPP/str_replace.cpp
+++ CPP/str_replace.cpp
@@ -4,10 +4,26 @@ std::string str_replace(std::string search, std::string replace, std::string sub
 {
   std::string str = subject;
   size_t pos = 0;
-  while((pos = str.find(search, pos)) != std::string::npos)
+  while ((pos = str.find(search, pos)) != std::string::npos)
   {
     str.replace(pos, search.length(), replace);
     pos += replace.length();
+  }
+  return str;
+}
+
+#include <vector>
+std::string str_replace(std::vector<std::string> searches, std::string replace, std::string subject)
+{
+  std::string str = subject;
+  for (std::string search : searches)
+  {
+    size_t pos = 0;
+    while ((pos = str.find(search, pos)) != std::string::npos)
+    {
+      str.replace(pos, search.length(), replace);
+      pos += replace.length();
+    }
   }
   return str;
 }
@@ -19,9 +35,14 @@ int main (void)
 {
   using namespace std;
   string s = "TestFalseTruetrue True, False, TRUE, TRUe, fAlse, false";
-  cout << str_replace("T", "F", s);
+  
+  cout << str_replace("T", "F", s) << endl;
+  
+  vector<string> search = {",", " "};
+  cout << str_replace(search, "_", s) << endl;
   
   return 0;
 }
 // Result:
 // FestFalseFruetrue Frue, False, FRUE, FRUe, fAlse, false
+// TestFalseTruetrue_True__False__TRUE__TRUe__fAlse__false
