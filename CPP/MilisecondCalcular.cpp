b145afeaed70763038b9051fe7c6ef3aa4f5dced
Hải Lê <dark.hades.1102@gmail.com>
2017-07-14 02:33:39 +0900

    Update MilisecondCalcular.cpp

--- CPP/MilisecondCalcular.cpp
+++ CPP/MilisecondCalcular.cpp
@@ -7,18 +7,15 @@
 
 int main ()
 {
-
   std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
   
   for (int i = 0; i < 1000; i++)
   {
     std::cout << "1";
   }
-  
   std::cout << std::endl;
 
   std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-
   std::chrono::duration<double, std::milli> time_span = t2 - t1;
 
   std::cout << "It took me " << time_span.count() << " milliseconds.";
