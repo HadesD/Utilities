613ada5f7076c56fb790df8f0e9990c88e57e5bf
Hải Lê <dark.hades.1102@gmail.com>
2017-07-18 16:33:06 +0900

    Update UsageOfSTDIN.cpp

--- CPP/UsageOfSTDIN.cpp
+++ CPP/UsageOfSTDIN.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
+// argc = count argument
+// argv = array value argument
 int main(int argc, char *argv[])
 {
-  // argc = count argument
-  // argv = array value argument
+  
 }
