8b97e4f367080685af01b988fdc7c1cf8e9a71ab
Hải Lê <dark.hades.1102@gmail.com>
2017-07-20 18:42:38 +0900

    Update and rename color_debug.cpp to color_debug.hpp

--- CPP/color_debug.hpp
+++ CPP/color_debug.hpp
@@ -3,14 +3,18 @@
 #include <stdio.h>
 #include <string>
 // https://stackoverflow.com/questions/2616906/how-do-i-output-coloured-text-to-a-linux-terminal
-enum class DEBUG_TYPE
+enum class DEBUG_LEVEL
 {
   INFO,
   ERROR,
   WARNING,
 }
 
-#define INFO(s) log(DEBUG_TYPE::INFO, s);
+#define INFO(s) log(DEBUG_LEVEL::INFO, s);
 
+void log(DEBUG_LEVEL l, std::string s)
+{
+  
+}
 
 #endif
