f3b3955b678768ce717acf5173f84fbd434892e7
Dark.Hades <dark.hades.1102@gmail.com>
2017-11-02 22:54:43 +0900

    Update str_split.cpp

--- CPP/str_split.cpp
+++ CPP/str_split.cpp
@@ -1,19 +1,22 @@
 #include <vector>
 #include <string>
-#include <sstream>
 
-std::vector<std::string> str_split(const std::string &str, const char delimiter)
+std::vector<std::string> split(const std::string &s, const char delim)
 {
-  std::vector<std::string> internal;
-  std::stringstream ss(str); // Turn the string into a stream.
-  std::string tok;
-  
-  while(std::getline(ss, tok, delimiter))
+  unsigned int i = 0;
+  auto pos = s.find(delim);
+  std::vector<std::string> v;
+  while (pos != std::string::npos) {
+    v.emplace_back(s.substr(i, pos-i));
+    i = ++pos;
+    pos = s.find(delim, pos);
+  }
+  if (pos == std::string::npos)
   {
-    internal.push_back(tok);
+     v.emplace_back(s.substr(i, s.length()));
   }
   
-  return internal;
+  return v;
 }
 
 // I don't recommend using the std namespace in production code.
@@ -23,7 +26,7 @@ std::vector<std::string> str_split(const std::string &str, const char delimiter)
 
 int main(/* int argc, char *argv[] */) {
   std::string myCSV = "one,two,three,four";
-  std::vector<std::string> sep = str_split(myCSV, ',');
+  std::vector<std::string> sep = split(myCSV, ',');
 
   // If using C++11 (which I recommend)
   /* for(const std::string &t : sep)
