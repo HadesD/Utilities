28522477cfb9c3696538b9e22074ccdfd903fdff
Dark.Hades <dark.hades.1102@gmail.com>
2018-08-24 17:04:03 +0900

    Update scan_port.py

--- Python/scan_port.py
+++ Python/scan_port.py
@@ -11,38 +11,39 @@ def TCP_connect(ip, port_number, delay, output):
     except:
         output[port_number] = ''
 
-
+MAX_THREAD = 1021
 
 def scan_ports(host_ip, delay):
 
     threads = []        # To run TCP_connect concurrently
     output = {}         # For printing purposes
 
+    PORT_FROM = 10000
+
+    PORT_TO = PORT_FROM + MAX_THREAD
+
     # Spawning threads to scan ports
-    for i in range(10000):
+    for i in range(PORT_FROM, PORT_TO):
         t = threading.Thread(target=TCP_connect, args=(host_ip, i, delay, output))
         threads.append(t)
 
     # Starting threads
-    for i in range(10000):
+    for i in range(MAX_THREAD):
         threads[i].start()
 
     # Locking the script until all threads complete
-    for i in range(10000):
+    for i in range(MAX_THREAD):
         threads[i].join()
 
     # Printing listening ports from small to large
-    for i in range(10000):
-        if output[i] == 'Listening':
-            print(str(i) + ': ' + output[i])
-
-
+    for i in range(MAX_THREAD):
+        if output[i+PORT_FROM] == 'Listening':
+            print(str(i+PORT_FROM) + ': ' + output[i+PORT_FROM])
 
 def main():
-    host_ip = input("Enter host IP: ")
-    delay = int(input("How many seconds the socket is going to wait until timeout: "))   
+    host_ip = raw_input("Enter host IP: ")
+    delay = int(input("How many seconds the socket is going to wait until timeout: "))
     scan_ports(host_ip, delay)
 
 if __name__ == "__main__":
     main()
-    
