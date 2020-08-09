6c88b12bcdb06c7209cac84a308e6068ab03e3fb
Ichiro <dark.hades.1102@gmail.com>
2020-08-10 00:31:46 +0900

    Create ConnectVPN.bat

---
+++ Batch/ConnectVPN.bat
@@ -0,0 +1,16 @@
+@echo OFF
+set VPN_NAME=vpn-for-game
+set SERVER_ADDR=sg-l2tp.racevpn.com
+set SHARED_KEY=racevpn
+set VPN_USERNAME=racevpn.com-vpnvpn1
+set VPN_PASSWORD=vpnvpn1
+
+rasdial /DISCONNECT
+
+powershell Add-VpnConnection -Name %VPN_NAME% -ServerAddress %SERVER_ADDR% -Force -Confirm:$false && ^
+powershell Set-VpnConnection -Name %VPN_NAME% -ServerAddress %SERVER_ADDR% -TunnelType L2tp -L2tpPsk %SHARED_KEY% -RememberCredential:$true -Force:$true -Confirm:$false
+
+powershell Get-VpnConnection -Name %VPN_NAME% && ^
+rasdial %VPN_NAME% %VPN_USERNAME% %VPN_PASSWORD%
+
+pause
