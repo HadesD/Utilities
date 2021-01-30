3acad2db98636a1d7e89656a54770aac56a99633
Ichiro <dark.hades.1102@gmail.com>
2021-01-30 23:19:26 +0900

    Update RunSomeContainer.sh

--- Docker/RunSomeContainer.sh
+++ Docker/RunSomeContainer.sh
@@ -1,4 +1,8 @@
-# MySQL
-docker run --restart unless-stopped --env="MYSQL_ALLOW_EMPTY_PASSWORD=ON" -p 3306:3306 mysql
+# mysql
+docker run -d --restart unless-stopped --name mysql --env="MYSQL_ALLOW_EMPTY_PASSWORD=ON" -p 3306:3306 mysql
 
-# 
+# php-fpm
+docker run -d --restart unless-stopped --name php -p 9000:9000 -v $PWD:/var/www/website php:7.4.14-fpm-alpine3.13
+
+# nginx
+docker run -d --restart unless-stopped --name nginx -p 9000:9000 -v $PWD:/var/www/website nginx
