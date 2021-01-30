094ae4346521cc6d389ef148f26ca419763d7022
Ichiro <dark.hades.1102@gmail.com>
2021-01-30 23:19:50 +0900

    Update RunSomeContainer.sh

--- Docker/RunSomeContainer.sh
+++ Docker/RunSomeContainer.sh
@@ -5,4 +5,4 @@ docker run -d --restart unless-stopped --name mysql --env="MYSQL_ALLOW_EMPTY_PAS
 docker run -d --restart unless-stopped --name php -p 9000:9000 -v $PWD:/var/www/website php:7.4.14-fpm-alpine3.13
 
 # nginx
-docker run -d --restart unless-stopped --name nginx -p 9000:9000 -v $PWD:/var/www/website nginx
+docker run -d --restart unless-stopped --name nginx -p 80:80 -v $PWD:/var/www/website nginx
