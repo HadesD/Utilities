4c8639ea892811f9ec37e0ca4a16d5e3e92d328a
Ichiro <dark.hades.1102@gmail.com>
2021-01-31 00:07:46 +0900

    Update RunSomeContainer.sh

--- Docker/RunSomeContainer.sh
+++ Docker/RunSomeContainer.sh
@@ -2,7 +2,7 @@
 docker run -d --restart unless-stopped --name mysql --env="MYSQL_ALLOW_EMPTY_PASSWORD=ON" -p 3306:3306 -p 33060:33060 mysql
 
 # php-fpm
-docker run -d --restart unless-stopped --name php -p 9000:9000 -v $PWD:/var/www/website php:7.4.14-fpm-alpine3.13
+docker run -d --restart unless-stopped --name php --link=mysql -p 9000:9000 -v $PWD:/var/www/website php:7.4.14-fpm-alpine3.13
 
 # nginx
-docker run -d --restart unless-stopped --name nginx -p 80:80 -v $PWD:/var/www/website nginx
+docker run -d --restart unless-stopped --name nginx --link=php -p 80:80 -v $PWD:/var/www/website -v $PWD/../nginx:/etc/nginx/conf.d nginx
