46f0b6c1ef473b5737d0c489498e466356bc4f10
Ichiro <dark.hades.1102@gmail.com>
2021-02-19 14:07:51 +0900

    Update GitBashGetPacman.sh

--- Shell/GitBashGetPacman.sh
+++ Shell/GitBashGetPacman.sh
@@ -1,10 +1,19 @@
-git clone --depth=1 https://github.com/git-for-windows/git-sdk-64 gfw-sdk
+REPO_DIR_NAME=gfw-sdk-64-t
+LIST_CHECKOUT_NEED="usr/bin/pacman.exe usr/bin/pacman-conf.exe usr/bin/pacman-db-upgrade usr/bin/pacman-key usr/bin/pacman-rec-filename-grep"
+LIST_CHECKOUT_NEED="$LIST_CHECKOUT_NEED etc/pacman.d/ var/lib/pacman/ usr/share/makepkg/util"
 
-cp gfw-sdk/usr/bin/pacman* /usr/bin/
-cp -a gfw-sdk/etc/pacman.* /etc/
+git clone --depth=1 --filter=blob:none --sparse https://github.com/git-for-windows/git-sdk-64.git --no-checkout ${REPO_DIR_NAME}
+cd ${REPO_DIR_NAME}
+git sparse-checkout init --cone
+git reset HEAD
+git checkout -- $LIST_CHECKOUT_NEED
+# git checkout -f $LIST_CHECKOUT_NEED
+
+cp usr/bin/pacman* /usr/bin/
+cp -a etc/pacman.* /etc/
 mkdir -p /var/lib/
-cp -a gfw-sdk/var/lib/pacman /var/lib/
-cp -a gfw-sdk/usr/share/makepkg/util* /usr/share/makepkg/
+cp -a var/lib/pacman /var/lib/
+cp -a usr/share/makepkg/util* /usr/share/makepkg/
 
 pacman --database --check
 
