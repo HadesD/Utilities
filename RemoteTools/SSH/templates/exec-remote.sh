182bb964bbd3d605812fc0689de3fc0e910c0891
Ichiro <dark.hades.1102@gmail.com>
2021-05-27 10:52:17 +0900

    Update exec-remote.sh

--- RemoteTools/SSH/templates/exec-remote.sh
+++ RemoteTools/SSH/templates/exec-remote.sh
@@ -12,29 +12,34 @@ if [[ "$CONN_TYPE" == "" ]]; then
 Default[1]: ' CONN_TYPE
 fi
 
-ENDPOINT_URL="${REMOTE_SERVER_USERNAME}:$REMOTE_SERVER_PASSWORD@${REMOTE_SERVER_HOST}:${REMOTE_SERVER_PORT}"
+DISPLAY=127.0.0.1:0
+
+ENDPOINT_URL="${REMOTE_SERVER_USERNAME}:${REMOTE_SERVER_PASSWORD}@${REMOTE_SERVER_HOST}:${REMOTE_SERVER_PORT}"
 
 if [[ "$CONN_TYPE" == "2" ]]; then
   echo [+] WinSCP to $ENDPOINT_URL
   PPK_KEY_FILE="../Keys/${REMOTE_SERVER_NAME}.ppk"
   PASSWORD_FLAG="-password=\"$REMOTE_SERVER_PASSWORD\""
+  WINE_EXE=''
+  WINEPATH_EXE=''
   if [ -x "$(command -v wine)" ]; then
-    if [ ! -f "${PPK_KEY_FILE}" ]; then
-      wine ../WinSCP/WinSCP.com /keygen "$(winepath --windows ${SSH_KEY_FILE})" /output="$(winepath --windows ${PPK_KEY_FILE})"
-    fi
-	if [ -f "${PPK_KEY_FILE}" ]; then
-		PASSWORD_FLAG="-privatekey="$(winepath --windows ${PPK_KEY_FILE})""
-	fi
-    wine ../WinSCP/WinSCP.exe $PASSWORD_FLAG -privatekey="$(winepath --windows ${PPK_KEY_FILE})" sftp://${REMOTE_SERVER_USERNAME}@${REMOTE_SERVER_HOST}:${REMOTE_SERVER_PORT} -sessionname="${REMOTE_SERVER_NAME}"
+    WINE_EXE=wine
+	WINEPATH_EXE='winepath --windows'
   else
-    if [ ! -f "${PPK_KEY_FILE}" ]; then
-      ../WinSCP/WinSCP.com -keygen "${SSH_KEY_FILE}" -output="${PPK_KEY_FILE}"
-    fi
-	if [ -f "${PPK_KEY_FILE}" ]; then
-		PASSWORD_FLAG="-privatekey="${PPK_KEY_FILE}""
-	fi
-    ../WinSCP/WinSCP.exe $PASSWORD_FLAG "sftp://${REMOTE_SERVER_USERNAME}@${REMOTE_SERVER_HOST}:${REMOTE_SERVER_PORT}" -sessionname="${REMOTE_SERVER_NAME}"
+    WINE_EXE=start
+    # WINE_EXE=''
   fi
+
+  if [ ! -f "${PPK_KEY_FILE}" ]; then
+    if [ -f "$SSH_KEY_FILE" ]; then
+      $WINE_EXE ../WinSCP/WinSCP.com /keygen "$($WINEPATH_EXE $SSH_KEY_FILE)" /output="$($WINEPATH_EXE ${PPK_KEY_FILE})"
+	fi
+  fi
+  if [ -f "${PPK_KEY_FILE}" ]; then
+    PASSWORD_FLAG="-privatekey="$($WINEPATH_EXE ${PPK_KEY_FILE})""
+  fi
+  $WINE_EXE ../WinSCP/WinSCP.exe $PASSWORD_FLAG sftp://${REMOTE_SERVER_USERNAME}:${REMOTE_SERVER_PASSWORD}@${REMOTE_SERVER_HOST}:${REMOTE_SERVER_PORT} -sessionname="${REMOTE_SERVER_NAME}"
+
 else
   SSH_APPEND_FLAGS="-XY -o ServerAliveInterval=30 ${REMOTE_SERVER_USERNAME}@${REMOTE_SERVER_HOST} -p ${REMOTE_SERVER_PORT}"
 
