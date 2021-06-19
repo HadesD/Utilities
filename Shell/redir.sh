cf4923bd834b3aeeff4b9391a790db960f111d0e
Ichiro <dark.hades.1102@gmail.com>
2021-06-19 17:15:20 +0900

    Create redir.sh

---
+++ Shell/redir.sh
@@ -0,0 +1,5 @@
+read -p 'REDIRECT_TO_CADDR: ' CADDR
+read -p 'REDIRECT_TO_CPORT: ' CPORT
+read -p "LISTEN_PORT[${CPORT}]: " LPORT
+
+redir --foreground --caddr="$CADDR" --lport=$LPORT --cport=$CPORT > "${LPORT}_${CADDR}_${CPORT}.log"
