53df9d2414d6e20f2cedde4bbe182bc0c3527b64
Ichiro <dark.hades.1102@gmail.com>
2021-06-19 17:33:08 +0900

    Update redir.sh

--- Shell/redir.sh
+++ Shell/redir.sh
@@ -1,5 +1,14 @@
-read -p 'REDIRECT_TO_CADDR: ' CADDR
-read -p 'REDIRECT_TO_CPORT: ' CPORT
-read -p "LISTEN_PORT[${CPORT}]: " LPORT
+while getopts l:h:p: flag
+do
+    case "${flag}" in
+        l) LPORT=${OPTARG};;
+        h) CADDR=${OPTARG};;
+        p) CPORT=${OPTARG};;
+    esac
+done
+
+#read -p 'REDIRECT_TO_CADDR: ' CADDR
+#read -p 'REDIRECT_TO_CPORT: ' CPORT
+#read -p "LISTEN_PORT[${CPORT}]: " LPORT
 
 redir -n --caddr="${CADDR}" --lport=${LPORT} --cport=${CPORT} > "${LPORT}_${CADDR}_${CPORT}.log"
