06d1c021fbe5ccc64d2ea3dce84432ae7a47a25e
Ichiro <dark.hades.1102@gmail.com>
2022-05-11 14:29:15 +0900

    Update GoogleSheet.Translate.js

--- Javascript/GoogleSheet.Translate.js
+++ Javascript/GoogleSheet.Translate.js
@@ -1,46 +1,52 @@
 function translateSheet() {
-  var sheets = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = sheets.getSheetByName("DetailDesign");
-  var range = sheet.getRange("A1:AG2000");
-  var values = range.getValues();
+  const sheets = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = sheets.getSheetByName('DetailDesign');
+  const range = sheet.getRange('A2:AG5');
+  const rangeRow = range.getRow();
+  const rangeColumn = range.getColumn();
+  const values = range.getValues();
   for (let rowI = 0; rowI < values.length; rowI++) {
     let row = values[rowI];
     for (let colI = 0; colI < row.length; colI++) {
-      
-      // if (rowI < 803) {
-      // if (rowI !== 806) {
-      //   continue;
-      // }
-
       let col = row[colI];
-      // Logger.log(col);
-      if (!col) {
+      if (!col || !col.length) {
         continue;
       }
 
-      if (!col.length || (col === '〇')) {
-        continue;
-      }
-
+      // If english already
       if (col.match(/^[a-zA-Z0-9-_. ]+$/g)) {
         continue;
       }
 
-      let toLang = "en_US";
+      // TODO: Customize here
+      if (col === '〇') {
+        continue;
+      }
 
+      const rowINum = rowI + rangeRow;
+      const colINum = colI + rangeColumn;
+
+      // Default will translate to English
+      let toLang = 'en_US';
+
+      // From line you want to translate to vi
       if (rowI >= 801) {
+        // If has dot character -> not translate
         if (col.indexOf('.') === -1) {
-          toLang = "vi";
+          toLang = 'vi';
         }
       }
 
-      let cell = sheet.getRange(rowI + 1, colI + 1);
+      const cell = sheet.getRange(rowINum, colINum);
+
+      Logger.log(cell.getValue());
+
+      continue;
       let hasLink = false;
       cell.getRichTextValue().getRuns().forEach(v => {
         if (hasLink) {
           return;
         }
-        //Logger.log(v.getLinkUrl());
         hasLink = v.getLinkUrl();
       });
 
