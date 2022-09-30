078d173922a882abfad51949da443d9dd0c05661
Ichiro <dark.hades.1102@gmail.com>
2022-09-30 19:29:06 +0900

    Update restart_app_with_pwd.sh

--- Shell/restart_app_with_pwd.sh
+++ Shell/restart_app_with_pwd.sh
@@ -5,5 +5,10 @@
 APP_GREP_NAME=$1
 APP_PWD=$2
 
+if [ "${APP_GREP_NAME}" == "" ] || [ "${APP_PWD}" == "" ]; then
+  echo 'You must enter param[0] = APP_GREP_NAME and param[1] = APP_PWD'
+  exit 1
+fi
+
 kill $(pwdx $(pgrep ${APP_GREP_NAME}) | grep "${APP_PWD}" | cut -f1 -d\:)
 ${APP_GREP_NAME}
