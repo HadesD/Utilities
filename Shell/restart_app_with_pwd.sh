da9e4791f056c4c09077c92608c725bb711cf3c4
Ichiro <dark.hades.1102@gmail.com>
2022-09-30 19:38:31 +0900

    Update restart_app_with_pwd.sh

--- Shell/restart_app_with_pwd.sh
+++ Shell/restart_app_with_pwd.sh
@@ -10,5 +10,19 @@ if [ "${APP_GREP_NAME}" == "" ] || [ "${APP_PWD}" == "" ]; then
   exit 1
 fi
 
+if [ ! -d "${APP_PWD}" ]; then
+  echo "Dir not found: ${APP_PWD}"
+  exit 1
+fi
+
+cd ${APP_PWD}
+
+if ! command -v ${APP_GREP_NAME} &> /dev/null; then
+  echo "Command not found: ${APP_GREP_NAME}"
+  exit 1
+fi
+
+echo "Restarting ${APP_GREP_NAME} in ${APP_PWD}"
+
 kill $(pwdx $(pgrep ${APP_GREP_NAME}) | grep "${APP_PWD}" | cut -f1 -d\:)
 ${APP_GREP_NAME}
