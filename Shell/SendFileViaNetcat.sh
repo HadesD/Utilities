3e8195d0e2632c55e90d858a6bdef6935af9557c
Ichiro <dark.hades.1102@gmail.com>
2024-05-06 22:50:22 +0900

    Update SendFileViaNetcat.sh

--- Shell/SendFileViaNetcat.sh
+++ Shell/SendFileViaNetcat.sh
@@ -1,6 +1,7 @@
 # More info: https://serverfault.com/a/783175
 
-# Send side
-tar -zcf - DIR_PATH | sudo nc -N -l 9998
+# Send side: Start compressing and pipe to netcat listen server
+tar -zcf - $DIR_PATH | sudo nc -N -l $PORT
 
-# Receive side
+# Receive side: Get data from send side then extract data
+nc -d $Send_side_IP $Send_side_PORT | tar -zxv
