7df2d9b667645db1aeaf59e1471b5aad2e112cd2
Ichiro <dark.hades.1102@gmail.com>
2024-05-07 22:21:08 +0900

    Update SendFileViaNetcat.sh

--- Shell/SendFileViaNetcat.sh
+++ Shell/SendFileViaNetcat.sh
@@ -3,6 +3,11 @@
 
 # Send side: Start compressing and pipe to netcat listen server
 sudo tar -zc $DIR_PATH | nc -N -l -p $PORT
-
 # Receive side: Get data from send side then extract data
 nc -d $Send_side_IP $Send_side_PORT | sudo tar -zxv
+
+# More example
+
+# Docker send image
+docker save $DOCKER_IMAGE_NAME | bzip2 | sudo nc -N -l -p $PORT
+nc -d $Send_side_IP $Send_Side_PORT | docker load
